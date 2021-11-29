7794560d5abaa611b34ee27f1492f597be32f8b9
Bytebuddha <patrickisgreene@protonmail.com>
2021-11-29 15:08:47 -0600

    fix spacing in build/config.mk

--- build/config.mk
+++ build/config.mk
@@ -8,15 +8,15 @@ CFLAGS += -Wall -ldl -std=c99 -lpthread -lm
 
 SOURCE_FILES += $(wildcard \
     $(SOURCEDIR)/*.c \
-		$(SOURCEDIR)/core/*.c \
-		$(SOURCEDIR)/systems/*.c \
-		$(SOURCEDIR)/components/*.c \
-                $(SOURCEDIR)/resources/*.c \
+    $(SOURCEDIR)/core/*.c \
+    $(SOURCEDIR)/systems/*.c \
+    $(SOURCEDIR)/components/*.c \
+    $(SOURCEDIR)/resources/*.c \
 )
 
 INCLUDE_FILES = $(wildcard \
     $(SOURCEDIR)/core/*.h \
-		$(SOURCEDIR)/systems/*.h \
-		$(SOURCEDIR)/components/*.h \
-		$(SOURCEDIR)/resources/*.h \
+    $(SOURCEDIR)/systems/*.h \
+    $(SOURCEDIR)/components/*.h \
+    $(SOURCEDIR)/resources/*.h \
 )
